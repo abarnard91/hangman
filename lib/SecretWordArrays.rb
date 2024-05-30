7da60c7b274562aed916dd15af7d7ca1eec2d2c9
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2024-05-30 08:50:47 -0500

    Update SecretWordArrays.rb

--- lib/SecretWordArrays.rb
+++ lib/SecretWordArrays.rb
@@ -1,11 +1,11 @@
-module SecretWordArrays
+module Secret_word_arrays
 
-  @@secretWordArray = Array.new #array that pulls words from 10000 words google text doc
-  @@tenthousandWords = File.readlines("google-10000-english-no-swears.txt")
-  @@tenthousandWords.each do |word|
+  @@secret_word_array = Array.new #array that pulls words from 10000 words google text doc
+  @@ten_thousand_words = File.readlines("google-10000-english-no-swears.txt")
+  @@ten_thousand_words.each do |word|
     word.strip! #words in text file have an extra space so its removed 
     if word.length >= 5 and word.length < 13 # then only words that are between 5-12 letters in length added to the secret word Array
-      @@secretWordArray << word
+      @@secret_word_array << word
     end
   end
 end
